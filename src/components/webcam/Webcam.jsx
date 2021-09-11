0f2f0cf7da0eb1f60ecaff7fda538886e52b080a
Isaias Cerezo <isaias_cerezo@itrsa.com.ar>
2021-09-11 05:39:49 -0300

    sendimg

--- src/components/webcam/Webcam.jsx
+++ src/components/webcam/Webcam.jsx
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import React, { useState } from 'react';
+import React, { useState, Component } from 'react';
 import Webcam from 'react-webcam';
 import { Button } from '@material-ui/core';
 import { Form } from 'formik';
@@ -14,21 +14,31 @@ const videoConstraints = {
 
 export const WebcamCapture = () => {
   const [image, setImage] = useState('');
+  const im = 'b';
   const webcamRef = React.useRef(null);
   const capture = React.useCallback(() => {
     const imageSrc = webcamRef.current.getScreenshot();
+    const im = webcamRef.current.getScreenshot();
     setImage(imageSrc);
+    submitImage(im);
   });
-  const submitImage = async (e) => {
-    e.preventDefault();
-    const res = await axios.post('http://localhost:3001/api/v1/test_image', {
-      idUser: '2',
-      idTest: '2',
-      imageTest: image,
-      itemPrueba: 'Hola'
-    });
-    console.log(res);
-    e.preventDefault();
+  const submitImage = async (img) => {
+    console.log(img);
+    const formData = new FormData();
+    formData.append('imageTest', img);
+    formData.append('idUser', '2');
+    formData.append('idTest', '2');
+    formData.append('content', '');
+    const res = await axios
+      .post('http://localhost:3001/api/v1/test_image', formData, {
+        headers: {
+          'content-type': 'multipart/form-data'
+        }
+      })
+      .then((res) => {
+        console.log(res.data);
+      })
+      .catch((err) => console.log(err));
   };
 
   return (
