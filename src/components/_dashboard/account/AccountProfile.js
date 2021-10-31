48a8fc4c8c7ed74215a1321f6589e73f245c6454
bebarbara <cerezo.barbara@gmail.com>
2021-10-31 13:38:35 -0300

    Account

--- src/components/_dashboard/account/AccountProfile.js
+++ src/components/_dashboard/account/AccountProfile.js
@@ -1,3 +1,4 @@
+import React, { useState, useEffect } from 'react';
 import moment from 'moment';
 import {
   Avatar,
@@ -7,53 +8,74 @@ import {
   CardActions,
   CardContent,
   Divider,
+  Container,
   Typography
 } from '@material-ui/core';
+// Components
+import Page from '../../Page';
 
-const user = {
-  avatar: '/static/images/avatars/avatar_6.png',
-  city: 'Los Angeles',
-  country: 'USA',
-  jobTitle: 'Senior Developer',
-  name: 'Katarina Smith',
-  timezone: 'GTM-7'
-};
+//
 
-const AccountProfile = (props) => (
-  <Card {...props}>
-    <CardContent>
-      <Box
-        sx={{
-          alignItems: 'center',
-          display: 'flex',
-          flexDirection: 'column'
-        }}
-      >
-        <Avatar
-          src={user.avatar}
-          sx={{
-            height: 100,
-            width: 100
-          }}
-        />
-        <Typography color="textPrimary" gutterBottom variant="h3">
-          {user.name}
-        </Typography>
-        <Typography color="textSecondary" variant="body1">
-          {`${user.city} ${user.country}`}
-        </Typography>
-        <Typography color="textSecondary" variant="body1">
-          {`${moment().format('hh:mm A')} ${user.timezone}`}
-        </Typography>
-      </Box>
-    </CardContent>
-    <Divider />
-    <CardActions>
-      <Button color="primary" fullWidth variant="text">
-        Upload picture
-      </Button>
-    </CardActions>
-  </Card>
-);
+const getUser = (setUser) =>
+  fetch('https://learning-zone-poc.herokuapp.com/api/v1/users/5')
+    .then((response) => response.json())
+    .then((json) => {
+      console.log('LZ ALL USERS response json', json);
+      setUser(json);
+    })
+    .catch((error) => {
+      console.error(error);
+    });
 
-export default AccountProfile;
+export default function AccountProfile() {
+  const [user, setUser] = useState([]);
+
+  useEffect(() => {
+    console.log('useEffect', user);
+    getUser(handleUser);
+  }, []);
+  const handleUser = (response) => {
+    console.log('set user', response);
+    setUser(response.user);
+  };
+  return (
+    <Page title="User | Learning-Zone">
+      <Container index>
+        <Card>
+          <CardContent>
+            <Box
+              sx={{
+                alignItems: 'center',
+                display: 'flex',
+                flexDirection: 'column'
+              }}
+            >
+              <Avatar
+                src={user.avatar}
+                sx={{
+                  height: 100,
+                  width: 100
+                }}
+              />
+              <Typography color="textPrimary" gutterBottom variant="h3">
+                {user.name}
+              </Typography>
+              <Typography color="textSecondary" variant="body1">
+                {`${user.city} ${user.country}`}
+              </Typography>
+              <Typography color="textSecondary" variant="body1">
+                {`${moment().format('hh:mm A')} ${user.timezone}`}
+              </Typography>
+            </Box>
+          </CardContent>
+          <Divider />
+          <CardActions>
+            <Button color="primary" fullWidth variant="text">
+              Modificar
+            </Button>
+          </CardActions>
+        </Card>
+      </Container>
+    </Page>
+  );
+}
